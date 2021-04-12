c6320eb6269249adb84c4d8d6ac32053f719b75e
Roberto Lunar <robertoclunarg@gmail.com>
2021-04-12 09:18:52 -0400

    configuracion del key.ts

--- app/dist/keys.js
+++ app/dist/keys.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.key = void 0;
-const host = "10.10.0.7"; //process.env.MYSQL_SERVER;
+const host = process.env.MYSQL_SERVER;
 const user = process.env.MYSQL_USER;
-const password = ".4C3r04dm1n"; //process.env.MYSQL_PW;
+const password = process.env.MYSQL_PW;
 const namedatabase = process.env.MYSQL_DB;
 const port = process.env.MYSQL_PORT;
 const conLimit = 10;
